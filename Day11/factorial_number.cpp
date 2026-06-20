1897bfeddd90267b8ab660a28c5f5b10201a75bb
Nikhil Choudhary <nikhil.choudhary2025@glbajajgroup.org>
2026-06-20 13:57:08 +0530

    add this

---
+++ Day11/factorial_number.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+long factorial(int n) {
+	if(n == 0 || n == 1)
+	   return 1;
+	return n * factorial(n - 1);
+}
+
+int main() {
+	int n = 6;
+	printf("Factorial of %d = %ld\n", n, factorial(n));
+	return 0;
+}
